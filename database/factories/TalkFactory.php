5f3be6c74f96ad871532c58340a9103f3f81d6b6
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-11-18 12:54:03 +0900

    Create Models with Blueprint

---
+++ database/factories/TalkFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Talk;
+use App\Models\Speaker;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TalkFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Talk::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(4),
+            'abstract' => $this->faker->text,
+            'speaker_id' => Speaker::factory(),
+        ];
+    }
+}
