0e6afa515d4f14b44a463d427a097729bda9aa8b
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2023-11-26 15:38:07 +0900

    Layouts

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -2,8 +2,25 @@
 
 namespace App\Models;
 
+use App\Models\Talk;
 use App\Enums\Region;
+use App\Models\Venue;
+use App\Models\Speaker;
+use Filament\Forms\Get;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Tabs\Tab;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\MarkdownEditor;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -33,5 +50,79 @@ class Conference extends Model
     public function talks(): BelongsToMany
     {
         return $this->belongsToMany(Talk::class);
+    }
+
+    public static function getForm(): array
+    {
+        return [
+        Tabs::make()
+            ->columnSpanFull()
+            ->tabs([
+                Tab::make('Conference Details')
+                ->schema([
+                    TextInput::make('name')
+                        ->columnSpanFull()
+                        ->label('Conference Name')
+                        ->default('My Conference')
+                        ->required()
+                        ->maxLength(60),
+                    MarkdownEditor::make('description')
+                        ->columnSpanFull()
+                        ->required(),
+                    DateTimePicker::make('start_date')
+                        ->native(false)
+                        ->required(),
+                    DateTimePicker::make('end_date')
+                        ->native(false)
+                        ->required(),
+                    Fieldset::make('Status')
+                        ->columns(1)
+                        ->schema([
+                    Select::make('status')
+                        ->options([
+                            'draft' => 'Draft',
+                            'published' => 'Published',
+                            'archived' => 'Archived',
+                        ])
+                        ->required(),
+                    Toggle::make('is_published')
+                        ->default(true),
+                    ])
+                ]),
+                Tab::make('Locations')
+                ->schema([
+                    Select::make('region')
+                        ->live()
+                        ->enum(Region::class)
+                        ->options(Region::class),
+                    Select::make('venue_id')
+                        ->searchable()
+                        ->preload()
+                        ->createOptionForm(Venue::getForm())
+                        ->editOptionForm(Venue::getForm())
+                        ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Get $get) {                                       
+                            return $query->where('region', $get('region'));
+                        }),
+                ]),
+            ]),
+        // Section::make('Conference Details')
+        // ->collapsible()
+        // // ->aside()
+        // ->description('Provide some basic information about the conference.')
+        // ->icon('heroicon-o-information-circle')
+        // ->columns(['md' => 2, 'lg' => 3]),
+
+        // Section::make('Location')
+        // ->columns(2),
+
+        CheckboxList::make('speakers')
+            ->columnSpanFull()
+            ->columns(3)
+            ->relationship('speakers', 'name')
+            ->options(
+                Speaker::all()->pluck('name', 'id')
+            )
+            ->required(),
+        ];
     }
 }
